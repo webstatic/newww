18cee2b08606bc98047f2d6ee745efc1c1fbe871
webstatic <rutapon@hotmail.com>
2022-05-13 23:00:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,14 @@ http = require('http');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
+// var logger = require('morgan');
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/quotes', quotesRouter);
+app.use(express.static(path.join(__dirname, 'web')));
 
 module.exports = app;
